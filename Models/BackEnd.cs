987e556cc639fa32993c6ef57541d6089b15ef12
Geoff Goetz and Zakkrey Short <zakkrey@>
2020-02-26 15:08:23 -0800

    fixed the dinosaur app

--- Models/BackEnd.cs
+++ Models/BackEnd.cs
@@ -1,16 +1,13 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace Jurassic.Models
 {
   public class Dino
   {
-    public static Dictionary<string, Dino> dinoGroup = new Dictionary<string, Dino>();
-
-    public void addDino(string species, string color, string diet, string name)
-    {
-      dinoGroup[name] = new Dino(species, color, diet);
-    }
+    public static List<Dino> dinosaursList = new List<Dino>()
+    {};
     private string _species;
     private string _color;
     private string _diet;
@@ -23,24 +20,36 @@ namespace Jurassic.Models
       _diet = diet;
       _name = name;
     }
-    public string GetSpecies()
+    
+    public static void AddDino()
     {
-      return _species;
+        Console.WriteLine("Please enter a Dinosaur species");
+        string _type = Console.ReadLine();
+        Console.WriteLine("Please enter a Dinosaur color");
+        string _color = Console.ReadLine();
+        Console.WriteLine("Please enter a Dinosaur diet");
+        string _diet = Console.ReadLine();
+        Console.WriteLine("Please give the dino a name");
+        string _name = Console.ReadLine();
+        Console.WriteLine("Dinosaur Added!");
+        Dino dinosaurs = new Dino(_type, _color, _diet, _name);
+        dinosaursList.Add(dinosaurs);
     }
 
-    public string GetColor()
+    public static void ViewDino()
     {
-      return _color;
-    }
-
-    public string GetDiet()
-    {
-      return _diet;
-    }
-
-    public string GetName()
-    {
-      return _name;
+      Console.WriteLine("What dino would you like to view");
+      string inputName = Console.ReadLine();
+      foreach(Dino data in dinosaursList)
+      {
+        if (inputName == data._name)
+        {
+          Console.WriteLine(data._species);
+          Console.WriteLine(data._color);
+          Console.WriteLine(data._diet);
+          Console.WriteLine(data._name);
+        }
+      }
     }
   }
 }
--- Wildlife.cs
+++ Wildlife.cs
@@ -1,52 +1,25 @@
 using System;
-using System.Collections.Generic;
 using Jurassic.Models;
 
 
-namespace Jurassic
+
+
+public class Program
 {
-  public class Program
+  public static void Main()
   {
-    private string _type;
-    private string _color;
-    private string _diet;
-    private string _name;
-    public static void Main()
+    Console.WriteLine("Would you like to:");
+    Console.WriteLine("[View] or [Add]");
+    string userChoice = Console.ReadLine();
+    if (userChoice == "add")
     {
-  // public static Dinosaur newDinosaur = new Dinosaur();
+      Dino.AddDino();
+      Main();
     }
-    public static void ChoosePath()
+    else if (userChoice == "view")
     {
-      Console.WriteLine("Would you like to:");
-      Console.WriteLine("[View] or [Add]");
-      string userChoice = Console.ReadLine();
-      if (userChoice == "add")
-      {
-        AddDino();
-      }
-      else if (userChoice == "view")
-      {
-
-      }
+      Dino.ViewDino();
+      Main();
     }
-    public static void AddDino()
-    {
-        Console.WriteLine("Please enter a Dinosaur species");
-        string _type = Console.ReadLine();
-        Console.WriteLine("Please enter a Dinosaur color");
-        string _color = Console.ReadLine();
-        Console.WriteLine("Please enter a Dinosaur diet");
-        string _diet = Console.ReadLine();
-        Console.WriteLine("Please give the dino a name");
-        string _name = Console.ReadLine();
-        // Console.WriteLine("Dinosaur Added!");
-    }
-
-    public static void ViewDino()
-    {
-      Console.WriteLine("What dino would you like to view");
-      Console.ReadLine(dinoGroup)
-    }
-
   }
 }
